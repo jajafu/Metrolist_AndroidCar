de1c5ac5890a7c31938116bc086b075c92dbad59
clifchi <clif.chi@gmail.com>
2026-08-06 08:27:49 +0800

    chore: bump version to 13.6.54

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 202
-        versionName = "13.6.53"
+        versionCode = 203
+        versionName = "13.6.54"
         resValue("string", "app_name", "Metrolist_AndroidCar")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
